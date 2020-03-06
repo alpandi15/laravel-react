9a9d15e8f4abfe4bce737456009d465948e11def
Muhammad Al-Pandi <mh2.alpandi@yahoo.co.id>
2020-03-06 11:23:25 +0700

    Create Api and handle request

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,39 +5,62 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\User;
+use App\Http\Requests\LoginRequest;
 
-class AuthController extends Controller
+class AuthController extends ApiController
 {
   function register (Request $request) {
-    $file_name = "";
-    if($file = $request->file('img_profil')){
-        $location_file = 'images/member/profile/';
-        $extensi = $file->getClientOriginalExtension();
-        $rand = rand(111, 999);
-        $name_file = $rand.'.'.$extensi;
-        $file->move($location_file, $name_file);
-        $file_name = $location_file.$name_file;
-    }
-    // return $file_name;
-    $user = User::create([
-        'name' => $request['name'],
-        'email' => $request['email'],
-        'username' => $request['username'],
-        'password' => bcrypt($request['password']),
-    ]);
+    try {
+        $file_name = "";
+        if($file = $request->file('img_profil')){
+            $location_file = 'images/member/profile/';
+            $extensi = $file->getClientOriginalExtension();
+            $rand = rand(111, 999);
+            $name_file = $rand.'.'.$extensi;
+            $file->move($location_file, $name_file);
+            $file_name = $location_file.$name_file;
+        }
+        // return $file_name;
+        $user = User::create([
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'username' => $request['username'],
+            'password' => bcrypt($request['password']),
+        ]);
 
-    return response()->json($user, 201);
+        $result['data'] = $user;
+        $result['status'] = true;
+    }
+    catch(\Exception $e){
+        $result['status'] = false;
+        $result['message'] = 'Operation failed due to '. $e->getMessage();
+    }
+
+    if($result['status']){
+        return $this->success($result['data']);
+    }else{
+        return $this->fail($result['message']);
+    }
   }
 
-  function login (Request $request) {
-    $this->validate($request, [
-        'account'   => 'required',
-        'password' => 'required|min:6'
-    ]);
+  function login (LoginRequest $request) {
+        try {
+            $account = $request->account;
+            $password = $request->password;
 
-    $account = $request->account;
-    $password = $request->password;
+            $result['data'] = $request->all();
+            $result['status'] = true;
+        }
+        catch(\Exception $e){
+            $result['status'] = false;
+            $result['message'] = 'Operation failed due to '. $e->getMessage();
+        }
 
+        if($result['status']){
+            return $this->success($result['data']);
+        }else{
+            return $this->fail($result['message']);
+        }
   }
 }
 
---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'account' => 'required',
+            'password' => 'required',
+        ];
+    }
+
+    /**
+     * Get the error messages that apply to the request parameters.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'account.required' => 'Account field is required',
+            'password.required' => 'Password field is required',
+        ];
+    }
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        $errors = (new ValidationException($validator))->errors();
+        throw new HttpResponseException(
+            response()->json([
+                'success'=>false,
+                'errors' => $errors
+            ], JsonResponse::HTTP_UNPROCESSABLE_ENTITY));
+    }
+}
